c9456a29ff2634ee2ebcf19efac4263b73850f8f
razevedo1994 <razevedo.contato@gmail.com>
2022-07-22 21:54:23 -0300

    add CI on GitHub Actions

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="Rodrigo Azevedo",
+    python_requires=">=3.8",
     packages=find_packages(),
     entry_points={"console_scripts": ["dundie = dundie.__main__:main"]},
     install_requires=read_requirements("requirements.txt"),
